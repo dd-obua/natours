e221363a1bf24a70ca209e7bb528252c688fd611
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-24 13:28:07 +0300

    Define route for get

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 
 const port = 3000;
 
+// Defined routes
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
