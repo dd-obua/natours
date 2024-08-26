cb9c5aa5ec604893356a5604ff07729e4027446f
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-26 12:35:44 +0300

    Define tours route with get method

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
-// Defined routes
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
-});
+const filePath = `${__dirname}/dev-data/data/tours-simple.json`;
+const encoding = 'utf-8';
+const tours = JSON.parse(fs.readFileSync(filePath, encoding));
 
-app.post('/', (req, res) => {
-  res.send('You can post to this end point');
+// Define tours route
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
 
 // Start server
