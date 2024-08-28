fd5521ac8c06de7849dbbaa2e31747867c7e5d84
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-28 20:49:46 +0300

    Remove unnecessary spaces

--- app.js
+++ app.js
@@ -7,25 +7,20 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middleware
-
 app.use(morgan('dev'));
-
 app.use(express.json()); // body parser
-
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
 });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// Mount routers on routes
 const toursUrl = '/api/v1/tours';
 const usersUrl = '/api/v1/users';
- 
-// Mount routers on routes
 app.use(toursUrl, tourRouter);
 app.use(usersUrl, userRouter);
 
