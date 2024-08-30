74f8519b818cc6cf685744dae018044b01cc7a13
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-30 19:14:42 +0300

    Link config.env file to the entire application

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
 // Start server
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
 });
