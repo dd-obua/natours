c36e4e8738b7b0e0951b1184de64b97544fd5a24
Obua Denis Daniel <dd.obua@gmail.com>
2024-08-30 17:46:41 +0300

    Create middleware for serving static files from public directory

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json()); // body parser
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require('./app');
 
 // Start server
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
 });
