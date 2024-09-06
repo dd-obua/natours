899c0e52adc67cdf0b1b79bd7a94e1c4f2b66b6a
Obua Denis Daniel <dd.obua@gmail.com>
2024-09-06 14:57:07 +0300

    Connect to the database using mongoose driver

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+// Connect to the database
+const connectionString = process.env.CONNECTION_STRING.replace(
+  '<db_name>',
+  process.env.DB_NAME
+).replace('<db_password>', encodeURIComponent(process.env.DB_PASSWORD));
+
+mongoose
+  .connect(connectionString)
+  .then((conn) => console.log('DB connection successful'));
 
 // Start server
 const port = process.env.port || 3000;
