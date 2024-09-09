1233de4d1da8c79289431b352c2c2ae9b4d7799a
Obua Denis Daniel <dd.obua@gmail.com>
2024-09-09 16:08:22 +0300

    Extract tour model into seperate file

---
+++ models/tourModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// mongoose schema
+const tourDefinition = {
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+};
+
+const tourSchema = new mongoose.Schema(tourDefinition);
+
+const Tour = mongoose.model(tourSchema);
+
+module.exports = Tour;
--- server.js
+++ server.js
@@ -18,35 +18,6 @@ mongoose
   .connect(connectionString)
   .then(() => console.log('DB connection successful'));
 
-// mongoose schema
-const tourDefinition = {
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-};
-
-const tourSchema = new mongoose.Schema(tourDefinition);
-
-// Tour model
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Tour data
-const tourData = {
-  name: 'The Park Camper',
-  price: 997,
-};
-
-const testTour = new Tour(tourData);
-
-testTour
-  .save()
-  .then((doc) => console.log(doc))
-  .catch((error) => console.log(error));
-
 // Start server
 const port = process.env.port || 3000;
 app.listen(port, () => {
