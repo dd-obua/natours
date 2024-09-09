22d1c6c1b0fa6ad8c7ee19ecef29ef251dbd8121
Obua Denis Daniel <dd.obua@gmail.com>
2024-09-09 14:20:54 +0300

    Create tours schema

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ mongoose
   .connect(connectionString)
   .then(() => console.log('DB connection successful'));
 
+// mongoose schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
 // Start server
 const port = process.env.port || 3000;
 app.listen(port, () => {
